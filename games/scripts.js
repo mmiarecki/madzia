78995740598bfad150a506ef4f3142208e18f218
mmiarecki <maja.miarecki@gmail.com>
2013-08-11 09:59:01 +0200

    addition done, still help needs to be done
    todo:
    - stats
    - store data in browser
    - rest communication with server to store data

--- games/scripts.js
+++ games/scripts.js
@@ -2,6 +2,7 @@ var Count = function() {
     var one = 1;
     var two = 2;
     var sign = "+";
+    var maxRange = 10;
     
     this.getOne = function() {
         return one;
@@ -31,6 +32,10 @@ var Count = function() {
         return jQuery('input.result').val();
     };
     
+    var getMaxRange = function() {
+    	return maxRange;
+    }
+    
     this.setCountEvent = function() {
         jQuery('button.count').on('click', function(event){
             event.preventDefault();
@@ -39,7 +44,7 @@ var Count = function() {
     };
     
     this.checkEquation = function() {
-        return (eval(this.getOne() + this.getSign() + this.getTwo()) == this.getResult());
+        return (eval(this.getOne() + this.getSign() + this.getTwo()) == this.getResult()); // res = '07'
     };
     
     this.createFeedback = function(result) {
@@ -51,15 +56,27 @@ var Count = function() {
     
     var positiveFeedback = function() {
         alert('Bardzo dobrze!!');
+        createNewEquation();
     };
     
     var helpFeedback = function() {
-        alert('Tu będzie podpowiedź');
+        alert('Niestety źle!');
     }
     
-    this.createNewEquation = function() {
-        
+    var createNewEquation = function() {
+    	var max = getMaxRange();
+        var a = Math.floor(Math.random() * max) + 1;
+        var b = Math.floor(Math.random() * (max-a)) + 1;
+        setOne(a);
+        setTwo(b);
+        showNewEquation();
     };
+    
+    var showNewEquation = function() {
+    	jQuery('span.firstNr').text(count.getOne());
+    	jQuery('span.secondNr').text(count.getTwo());
+    	jQuery('input.result').val('');
+    }
 }
 
 var count = new Count();
